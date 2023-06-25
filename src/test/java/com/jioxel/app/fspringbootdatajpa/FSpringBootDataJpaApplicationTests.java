63a0f8d024b456d20f18180b36ae6c4ca05a5d74
Jovany <jovanyevs@gmail.com>
2023-06-25 08:30:59 -0600

    first commit

---
+++ src/test/java/com/jioxel/app/fspringbootdatajpa/FSpringBootDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jioxel.app.fspringbootdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FSpringBootDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
